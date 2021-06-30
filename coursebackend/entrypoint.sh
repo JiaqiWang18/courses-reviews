9d609028ecb88bbb2987e89d1a01f587cf5126ea
Jiaqi (Jacky) Wang <jacky200218@gmail.com>
2021-06-29 18:03:51 -0700

    deploy

--- coursebackend/entrypoint.sh
+++ coursebackend/entrypoint.sh
@@ -1,1 +1,1 @@
-python manage.py collectstatic --no-input && gunicorn coursebackend.wsgi -b 0.0.0.0:8000
+python manage.py collectstatic --no-input && gunicorn --workers=4 --bind=0.0.0.0:8000 coursebackend.wsgi
