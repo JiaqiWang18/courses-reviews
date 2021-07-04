99ac0697d9678ab4a427930312cb89a2409a8dcf
Jiaqi (Jacky) Wang <jacky200218@gmail.com>
2021-07-03 17:12:07 -0700

    files

--- coursebackend/entrypoint.sh
+++
@@ -1,3 +0,0 @@
-python manage.py migrate --no-input
-python manage.py collectstatic --no-input
-gunicorn coursebackend.wsgi --bind 0.0.0.0:8000 --workers 4 --threads 4
--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 if ! [ -x "$(command -v docker-compose)" ]; then
   echo 'Error: docker-compose is not installed.' >&2
@@ -7,8 +7,8 @@ fi
 
 domains=(coursesreviews.net www.coursesreviews.net)
 rsa_key_size=4096
-data_path="./docker/nginx/certbot"
-email="" # Adding a valid address is strongly recommended
+data_path="./nginx/certbot"
+email="jacky200218@gmail.com" # Adding a valid address is strongly recommended
 staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
