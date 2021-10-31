69b9f9ca4e7bc9abe99fd1b352da733f8196b9b8
Jiaqi (Jacky) Wang <jacky200218@gmail.com>
2021-10-31 18:05:30 -0400

    use func cache cuz it wont cache on client

--- coursebackend/api/urls.py
+++ coursebackend/api/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from django.views.decorators.cache import cache_page
 from . import views
 
 router = DefaultRouter()
