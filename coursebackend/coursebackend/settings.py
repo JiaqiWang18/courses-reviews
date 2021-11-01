0011fd988edd6509b79fa0193238e90d81354928
Jiaqi (Jacky) Wang <jacky200218@gmail.com>
2021-11-01 01:58:01 -0400

    change token time

--- coursebackend/coursebackend/settings.py
+++ coursebackend/coursebackend/settings.py
@@ -153,7 +153,7 @@ REST_FRAMEWORK = {
 AUTH_USER_MODEL = 'user.User'
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=1),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
     'REFRESH_TOKEN_LIFETIME': timedelta(minutes=5),
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': True,
